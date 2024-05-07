9ab7b91de2868aa42310e12dabbaf808ccf73c11
Cloud9Minds <169011751+Cloud9Minds@users.noreply.github.com>
2024-05-07 17:15:09 +0800

    Update calldisc.js
    awd

--- calldisc.js
+++ calldisc.js
@@ -68,6 +68,18 @@ discRow.append(discDivFu);
 console.log(discContainer);
 console.log(discRow);
 
+const fnameContainer = document.getElementById("#contact.first_name");
+const fnameDiv = fnameContainer.children[1];
+const fnameInner = fnameDiv.children[1];
+const fname = fnameInner.children[2]
+
+const lnameContainer = document.getElementById("#contact.last_name");
+const lnameDiv = lnameContainer.children[1];
+const lnameInner = lnameDiv.children[1];
+const lname = lnameInner.children[2]
+
+console.log(fname +"" +lname);
+
 // Add event listener to links
 function discCbAction(){
   const fnameContainer = document.getElementById("#contact.first_name");
