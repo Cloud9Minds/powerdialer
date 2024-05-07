299b733da551ba5f6186e015ebb678123c72b02d
Cloud9Minds <169011751+Cloud9Minds@users.noreply.github.com>
2024-05-07 20:22:08 +0800

    Update calldisc.js

--- calldisc.js
+++ calldisc.js
@@ -68,13 +68,12 @@ discRow.append(discDivFu);
 console.log(discContainer);
 console.log(discRow);
 
-const fnamefff = document.querySelector('input[name="contact.first_name"]').value;
-const lname = document.querySelector('input[name="contact.last_name"]').value;
-console.log(fnamefff+" "+lname);
 
 // Add event listener to links
 function discCbAction(){
-
+  const fname = document.querySelector('input[name="contact.first_name"]').value;
+  const lname = document.querySelector('input[name="contact.last_name"]').value;
+  console.log(fname+" "+lname);
 }
 
 
